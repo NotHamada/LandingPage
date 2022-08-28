57c6eb56da3ef2dd852578cd008ec59f07ade0df
NotHamada <matheushamada@live.com>
2022-08-28 14:25:03 -0300

    Section successfully placed.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function toggleSidebar() {
     var sidebar = document.querySelector("#sidebar");
     var header = document.querySelector("#header");
+    var content = document.querySelector("#content");
     if (sidebar.className == 'sidebar-open') {
         sidebar.className = 'sidebar-close';
         header.className = 'header-unchanged';
+        content.className = 'content-unchanged';
     } else if (sidebar.className == 'sidebar-close') {
         sidebar.className = 'sidebar-open';
         header.className = 'header-changed';
+        content.className = 'content-changed';
     }
 }
