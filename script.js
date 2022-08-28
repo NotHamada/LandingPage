942d398a90fd6d8e920d6604e2c19d960b84f286
NotHamada <matheushamada@live.com>
2022-08-28 15:07:06 -0300

    Second section.

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ function toggleSidebar() {
     var sidebar = document.querySelector("#sidebar");
     var header = document.querySelector("#header");
     var content = document.querySelector("#content");
+    var work = document.querySelector("#work");
     if (sidebar.className == 'sidebar-open') {
         sidebar.className = 'sidebar-close';
         header.className = 'header-unchanged';
         content.className = 'content-unchanged';
+        work.className = 'work-unchanged';
     } else if (sidebar.className == 'sidebar-close') {
         sidebar.className = 'sidebar-open';
         header.className = 'header-changed';
         content.className = 'content-changed';
+        work.className = 'work-changed';
     }
 }
